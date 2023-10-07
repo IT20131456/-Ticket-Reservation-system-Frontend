adb5165a426911e09400f6b912d2788dac1f2405
IT20131456 <it20131456@my.sliit.lk>
2023-10-07 23:42:34 +0530

    Update Navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Ticket Booking Management/Text";
+
+// Import Navbar
+import Backoffice from "./components/Navbar/Backoffice";
+import TravelAgent from "./components/Navbar/Travel Agent";
+
+// Import Ticket Booking Management Componets
+import TicketBooking from "./components/Ticket Booking Management/NewReservations";
 
 function App() {
-  
   return (
     <BrowserRouter>
- 
-    {/* <NavBar/> */}
-
       <Routes>
-        {/* ---------------Admin--------------- */}
-        <Route path="/" element={<Home />} />
-        
+        <Route path="/backoffice" element={<Backoffice />} />
+        <Route path="/travelagent" element={<TravelAgent />} />
 
+        {/* Ticket Booking Management */}
+        <Route path="/ticketbooking" element={<TicketBooking />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/Ticket Booking Management/NewReservations.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TravelAgentNavBar from '../Navbar/Travel Agent'
+
+export default function NewReservations() {
+  return (
+    
+    <div>
+        <TravelAgentNavBar/>
+        NewReservations
+        </div>
+  )
+}
